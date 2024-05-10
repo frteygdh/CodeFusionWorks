9a6e65121190a95e47026d29e21d9a0d0ad05a85
frteygdh <gs1aldenash@hotmail.com>
2024-05-10 19:41:57 +0700

    init project

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const average = (arr) => arr.reduce((a, v) => a + v, 0) / arr.length;
+const quickSortRandomPivot = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivotIndex = Math.floor(Math.random() * arr.length);
+  const pivot = arr[pivotIndex];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== pivotIndex) {
+      if (arr[i] <= pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+  }
+  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
+};
