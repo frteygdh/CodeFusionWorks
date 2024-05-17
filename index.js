5f360e6187d492cbfeabaacb8c2d0956102413d4
frteygdh <gs1aldenash@hotmail.com>
2024-05-17 12:46:20 +0700

    feat

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
+function maxArea(height) {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    maxArea = Math.max(
+      maxArea,
+      Math.min(height[left], height[right]) * (right - left),
+    );
+    if (height[left] < height[right]) left++;
+    else right--;
+  }
+  return maxArea;
 }
